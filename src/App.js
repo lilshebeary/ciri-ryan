db4870eb13edf37ba61e27762276b5143505c167
Ciri Ryan <cirijaye@yahoo.com>
2022-03-08 05:05:12 -0800

    possible design

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
-
-import './App.css';
+import "./App.css";
+import bg from './background.svg'
 
 function App() {
   return (
     <div className="App">
-      <div className='name-title'>
+      
+      <div className="nav">
+        <nav className="navbar">
+          <li class="nav-item">
+            <a class="nav-link" href="./About.js">
+            <button type="button" class="btn btn-outline-dark rounded-pill lg">About </button>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/Portfolio.js">
+            <button type="button" class="btn btn-outline-dark rounded-pill">Portfolio </button>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/Contact.js">
+            <button type="button" class="btn btn-outline-dark rounded-pill "> Contact </button>
+            </a>
+          </li>
+        </nav>
+      </div>
+      <hr className="hr" />
+<section>
+      <div className="name-title">
         <h1>Ciri Ryan</h1>
         <h2>Software Developer</h2>
       </div>
-      <div className="nav">
-      <nav className="navbar">
-      <li class="nav-item">
-          <a class="nav-link" href="./About.js">About</a>
-        </li>
-      <li class="nav-item">
-          <a class="nav-link" href="/Portfolio.js">Portfolio</a>
-        </li>
-      <li class="nav-item">
-          <a class="nav-link" href="/Contact.js">Contact</a>
-        </li>
-      </nav>
-    </div>
-        <hr className='hr'/>
-     
+ </section>
     </div>
   );
 }
