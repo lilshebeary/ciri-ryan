116b95313b134fac0a6e4b04e6d8924bf8dd0076
Ciri Ryan <cirijaye@yahoo.com>
2022-03-24 12:23:20 -0700

    change path to home

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <Router>
      <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/ciri-ryan' element={<Home />} />
      </Routes>
      </Router>
   );
