935a26056c57b18b4ea72718ba3258d32493b833
Ciri Ryan <cirijaye@yahoo.com>
2022-05-10 07:15:17 -0700

    new build fix email

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -15,7 +15,7 @@ const Footer = () => {
         <SectionTitle>Contact</SectionTitle>
         <ContactText>
           Want to get in touch? Drop me an email at{" "}
-          <a href="mailto:cirijaye@yahoo.com">myemail@gmail.com</a>
+          <a href="mailto:cirijaye@yahoo.com">cirijaye@yahoo.com</a>
         </ContactText>
 
         <FooterSocials>
